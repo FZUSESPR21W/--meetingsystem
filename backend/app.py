6843223dfe072bb60ddb7cd0dab154a14538bb0c
Elsa1226 <76643161+Elsa1226@users.noreply.github.com>
2021-03-27 17:35:07 +0800

    增加api

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,60 @@
-from flask import Flask
+from flask import Flask,request
+from backend.Util.token import create_token, validate_token
+from backend.Database.database import Data
 
 app = Flask(__name__)
+data = Data()
+
+@app.route('/front',methods="GET")
+def index():
+    return
+
+@app.route('/backend',methods="GET")
+def index():
+    return
+
+@app.route('/api/user/meeting',methods="GET")
+def message():
+    return
+
+@app.route('/api/user/forum/message',methods="POST")
+def message():
+    token = request.form.get("token")
+    page = request.form.get("page")
+    id = request.form.get("id")
+    if id is None:
+        return
+    return
+
+@app.route('/api/user/query/follow',methods="POST")
+def ask_follow():
+    token = request.form.get("token")
+    id = request.form.get("id")
+    return
+
+@app.route('/api/user/forum/list',methods="POST")
+def forum_list():
+    token = request.form.get("token")
+    return
+
+@app.route('/api/user/follow/',methods="POST")
+def follow():
+    token = request.form.get("token")
+    follow_key = request.form.get("follow_key")
+    ids = request.form.get("ids")
+    return
+
+@app.route('/api/user/register',methods="POST")
+def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    email = request.form.get("email")
+    return
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+
+
 
 
 if __name__ == '__main__':
