cd6aea9554a0c1231a0e0840607cd84344aca561
GONGHAIXU <3111398755@qq.com>
2021-03-27 17:07:32 +0800

    feat: complete mail module

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,11 @@
 from flask import Flask
 
-from backend.Util.token import create_token, validate_token
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    token = create_token(123)
-    payload = validate_token(token)
-    return payload
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
