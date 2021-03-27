c203104e9dcee64990ed741f84568e4df6137e7c
tkhhhh <45356508+tkhhhh@users.noreply.github.com>
2021-03-27 20:20:18 +0800

    feat:完成发布消息，获取所管理论坛

--- backend/Database/database.py
+++ backend/Database/database.py
@@ -10,3 +10,34 @@ class Data(object):
 
     def __del__(self):
         self.db.close()
+
+
+    #获取所管理的论坛
+    def get_forum_charge(self,admin_id):
+        with self.db.cursor() as cursor:
+            sql = "SELECT sub_forum_id FROM role WHERE user_id=%s"
+            cursor.execute(sql, (admin_id))
+            forum_id = self.db.commit()
+            sql = "SELECT issue FROM sub_forum WHERE sub_forum_id=%s"
+            cursor.execute(sql,(forum_id))
+            issue = self.db.commit()
+            cursor.close()
+            result = [{
+                "id":forum_id,
+                "forum":issue
+            }]
+            return result
+
+    #发布消息
+    def publish_message(self,id,content,admin_id):
+        with self.db.cursor() as cursor:
+            sql = "INSERT INTO `message` (`sub_forum_id`,`content`) VALUES (%s,%s)"
+            cursor.execute(sql, (id,content))
+            self.db.commit()
+            cursor.close()
+            return True
+
+
+if __name__ == "__main__" :
+    db = Data()
+    db.get_forum_charge(1)
